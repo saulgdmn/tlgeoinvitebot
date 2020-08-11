e403fe6c12ee4572852f34011075285b8fd5177d
saul <saul@email>
2020-08-11 13:59:47 +0300

    0.1

--- database.py
+++ database.py
@@ -69,7 +69,7 @@ class SpectatedChat(BaseModel):
     def get_personal_referral_records(self, from_user):
         return ReferralRecord\
             .select()\
-            .where(ReferralRecord.from_user == from_user, ReferralRecord.joined_chat)\
+            .where(ReferralRecord.from_user == from_user, ReferralRecord.joined_chat == True)\
             .count()
 
 
